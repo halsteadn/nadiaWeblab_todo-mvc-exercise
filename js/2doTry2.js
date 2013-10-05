5bf85c4115931b592bb819beae61e1f446dcce0d
halsteadn <nadiahalstead@Nadias-MacBook-Air.local>
2013-10-05 11:51:35 -0600

    Floundering with Functions
    So I tried writing something using the .val('') advice — three
    different ways etc. Apparently nothing changes anything, but at least I
    fearlessly practiced something and after all, everything is everything,
    so I feel like I am one baby step farther along the steep learning
    curve…  (I hope this is not a misuse of what this comments box is meant
    for) :-\

--- js/2doTry2.js
+++ js/2doTry2.js
@@ -7,3 +7,20 @@ var nextItem = '<div class="somethingtoDo"><input type="checkbox" maxlength="40"
 function handleEvent1() {
     $("#Mainbox").append(nextItem);
 }
+
+function getSomethingdone() {
+    $(".somethingtoDo").val("");
+} 
+
+function getSomethingdone() {
+    $(label).val("");
+} 
+
+function getSomethingdone() {
+    $("#Mainbox").val("");
+} 
+
+function getSomethingdone() {
+    $(".todoInputtext").val("");
+} 
+
