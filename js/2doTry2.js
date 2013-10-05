ec5c561b2ab8eeec17eb24337a9e8510068ffbef
halsteadn <nadiahalstead@Nadias-MacBook-Air.local>
2013-10-05 10:58:35 -0600

    still working basix

--- js/2doTry2.js
+++ js/2doTry2.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-                  $("#inputId1").change(handleEvent1);
+    $("#inputId1").change(handleEvent1);
 });
 
-var nextItem = '<div class="somethingtoDo"><input type="checkbox"><label>(".labelText")</label><button></button></div>';
-function handleEvent1(){
-                  $("#Mainbox").append(nextItem);
+var nextItem = '<div class="somethingtoDo"><input type="checkbox" maxlength="40"><label>(".labelText")</label><button></button></div>';
+
+function handleEvent1() {
+    $("#Mainbox").append(nextItem);
 }
