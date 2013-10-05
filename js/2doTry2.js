608371c5043395e5cf412639fb5e94af0947778c
halsteadn <nadiahalstead@Nadias-MacBook-Air.local>
2013-10-05 12:08:20 -0600

    Aha! That was my problem
    "...I forgot to twitch!"
    —Tigger (in Playing Pooh Sticks)

--- js/2doTry2.js
+++ js/2doTry2.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $("#inputId1").change(handleEvent1);
 });
 
-var nextItem = '<div class="somethingtoDo"><input type="checkbox" maxlength="40"><label>("")</label><button></button></div>';
+var nextItem = '<div class="somethingtoDo"><input type="checkbox" maxlength="40"><label>("cantBedone")</label><button></button></div>';
 
 function handleEvent1() {
     $("#Mainbox").append(nextItem);
